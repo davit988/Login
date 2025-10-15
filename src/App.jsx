8321ffbaec0de5c01d507f73980e40aa8fb73d88
davit988 <archehovsepyan@gmail.com>
2025-10-16 00:19:37 +0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Regest } from './Components/Regest/Regest'
+
+function App() {
+
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default App
---
+++ src/Components/Regest/Regest.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+import './Regest.css'
+
+export const Regest = () => {
+  return (
+    <></>
+  )
+}
