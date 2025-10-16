0bc1296ec4d3df5a66de4686f8b2943e815961e7
davit988 <archehovsepyan@gmail.com>
2025-10-16 20:41:24 +0400

    end errors

--- src/Components/Regest/Regest.jsx
+++ src/Components/Regest/Regest.jsx
@@ -1,65 +1,85 @@
 import React from "react";
 import { Formik } from "formik";
+import * as YUP from "yup";
 
 import "./Regest.css";
 
 export const Regest = () => {
+  const validate = YUP.object().shape({
+    name: YUP.string().min(3, "min lenght").required("filed is requered"),
+    userName: YUP.string().min(3, "min lenght").required("filed is requered"),
+    email: YUP.string().email().required("filed is requered"),
+    pass: YUP.string().min(3, "min lenght").required("filed is requered"),
+    retPass: YUP.string().required("wrong pass"),
+  });
   return (
     <header>
       <div className="container">
         <Formik
           initialValues={{
             name: "",
-            srName: "",
+            userName: "",
             email: "",
             pass: "",
             retPass: "",
           }}
           onSubmit={(values) => console.log(values)}
+          validationSchema={validate}
         >
-          {({ values, handleChange, handleSubmit }) => (
+          {({ values, handleChange, handleSubmit, errors, touched }) => (
             <div className="regest">
               <form onSubmit={handleSubmit}>
                 <h1>Regest</h1>
-                <div>
+                <div className="reg">
                   <p>Name</p>
                   <input
                     value={values.name}
                     onChange={handleChange}
                     name="name"
+                    placeholder="Write Your Name"
                   />
+                  {errors.name && touched.name && <p>{errors.name}</p>}
                 </div>
-                <div>
-                  <p>srName</p>
+                <div className="reg">
+                  <p>userName</p>
                   <input
-                    value={values.srName}
+                    value={values.userName}
                     onChange={handleChange}
-                    name="srName"
+                    name="userName"
+                    placeholder="Write Your User Name"
                   />
+                  {errors.userName && touched.userName && <p>{errors.userName}</p>}
                 </div>
-                <div>
+                <div className="reg">
                   <p>email</p>
                   <input
                     value={values.email}
                     onChange={handleChange}
                     name="email"
+                    placeholder="Write Your email"
                   />
+                  {errors.email && touched.email && <p>{errors.email}</p>}
                 </div>
-                <div>
+                <div className="reg">
                   <p>Pass</p>
                   <input
                     value={values.pass}
                     onChange={handleChange}
                     name="pass"
+                    placeholder="Password"
                   />
+                  {errors.pass && touched.pass && <p>{errors.pass}</p>}
                 </div>
-                <div>
+                <div className="reg">
                   <p>RetPass</p>
                   <input
                     value={values.retPass}
                     onChange={handleChange}
                     name="retPass"
+                    placeholder="Ret Password"
                   />
+
+                  {console.log(values.pass)}
                 </div>
                 <button type="sumbit">Regest</button>
               </form>
