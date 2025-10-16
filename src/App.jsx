6ed92e328b187f0c3ca987fe708662fe7b673123
davit988 <archehovsepyan@gmail.com>
2025-10-16 18:14:49 +0400

    end regest block

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import './App.css'
-import { Regest } from './Components/Regest/Regest'
+import "./App.css";
+import { Formik } from "formik";
+import { Regest } from "./Components/Regest/Regest";
+import * as YUP from "yup";
 
 function App() {
+  // const validateSchema = YUP.object().shape({
+  //   login: YUP.string()
+  //     .min(3, " min lenght")
+  //     .max(7, "max lenght")
+  //     .required("filed is requered"),
+  // });
 
   return (
-   <>
-   
-   </>
-  )
+    <>
+      <Regest />
+      {/* <Formik
+        initialValues={{
+          login: "",
+        }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validateSchema}
+      >
+        {({ values, handleChange, handleSubmit, errors, touched }) => (
+          <form onSubmit={handleSubmit}>
+            <input value={values.login} onChange={handleChange} name="login" />
+            {
+              errors.login && touched.login && <p>{errors.login}</p>
+            }
+            <button type="sumbit">Login</button>
+          </form>
+        )}
+      </Formik> */}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Regest/Regest.jsx
+++ src/Components/Regest/Regest.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Formik } from "formik";
 
-
-import './Regest.css'
+import "./Regest.css";
 
 export const Regest = () => {
   return (
-    <></>
-  )
-}
+    <header>
+      <div className="container">
+        <Formik
+          initialValues={{
+            name: "",
+            srName: "",
+            email: "",
+            pass: "",
+            retPass: "",
+          }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ values, handleChange, handleSubmit }) => (
+            <div className="regest">
+              <form onSubmit={handleSubmit}>
+                <h1>Regest</h1>
+                <div>
+                  <p>Name</p>
+                  <input
+                    value={values.name}
+                    onChange={handleChange}
+                    name="name"
+                  />
+                </div>
+                <div>
+                  <p>srName</p>
+                  <input
+                    value={values.srName}
+                    onChange={handleChange}
+                    name="srName"
+                  />
+                </div>
+                <div>
+                  <p>email</p>
+                  <input
+                    value={values.email}
+                    onChange={handleChange}
+                    name="email"
+                  />
+                </div>
+                <div>
+                  <p>Pass</p>
+                  <input
+                    value={values.pass}
+                    onChange={handleChange}
+                    name="pass"
+                  />
+                </div>
+                <div>
+                  <p>RetPass</p>
+                  <input
+                    value={values.retPass}
+                    onChange={handleChange}
+                    name="retPass"
+                  />
+                </div>
+                <button type="sumbit">Regest</button>
+              </form>
+            </div>
+          )}
+        </Formik>
+      </div>
+    </header>
+  );
+};
